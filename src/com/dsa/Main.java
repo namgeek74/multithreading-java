2993c45b18d4656e8d36f67bcec597f01ab4a190
namnd <namnguyennd7428@gmail.com>
2023-10-02 14:50:47 +0700

    create new thread with runnable interface

--- src/com/dsa/Main.java
+++ src/com/dsa/Main.java
@@ -9,7 +9,16 @@ public class Main {
             public void run() {
                 // code that will run a new thread
                 System.out.println("We are now in thread: " + Thread.currentThread().getName());
-
+                System.out.println("Current thread priority is " + Thread.currentThread().getPriority());
+                throw new RuntimeException("Intentional Exception");
+            }
+        });
+        thread.setName("New Worker Thread");
+        thread.setPriority(Thread.MAX_PRIORITY);
+        thread.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                System.out.println("A critical error happened in thread " + t.getName() + " the error is " + e.getMessage());
             }
         });
 
